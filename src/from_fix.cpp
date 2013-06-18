f6afaa4c28b2f0c477e7f8da5d0c02d234e4cc51
Mike Purvis <mpurvis@clearpathrobotics.com>
2013-06-18 11:43:39 -0400

    Add some explanatory comments in preparation for release.

--- src/from_fix.cpp
+++ src/from_fix.cpp
@@ -7,6 +7,7 @@
 #include <boost/bind.hpp>
 
 extern "C" {
+  // The backend of this node is an included C library called libswiftnav.
   #include "coord_system.h"
 }
 
@@ -19,6 +20,12 @@ void initialize_datum(double datum_ecef[3],
 {
   ros::NodeHandle n("~");
   sensor_msgs::NavSatFix datum_msg(*fix_ptr);
+
+  // Local ENU coordinates are with respect to a plane which is 
+  // prependicular to a particular lat/lon. This logic decides 
+  // whether to use a specific passed-in point (typical for 
+  // repeated tests in a locality) or just an arbitrary starting
+  // point (more ad-hoc type scenarios).
   if (n.hasParam("datum_latitude") &&
       n.hasParam("datum_longitude") && 
       n.hasParam("datum_altitude")) {
@@ -31,6 +38,9 @@ void initialize_datum(double datum_ecef[3],
   }
   pub_datum.publish(datum_msg);
 
+  // The datum point is stored as an ECEF, for mathematical reasons.
+  // We convert it here, using the appropriate function from
+  // libswiftnav.
   double llh[3] = { datum_msg.latitude * TO_RADIANS,
                     datum_msg.longitude * TO_RADIANS,
                     datum_msg.altitude };
@@ -50,12 +60,16 @@ static void handle_fix(const sensor_msgs::NavSatFixConstPtr fix_ptr,
     have_datum = true;
   }
 
+  // Prepare the appropriate input vector to convert the input latlon
+  // to an ECEF triplet.
   double llh[3] = { fix_ptr->latitude * TO_RADIANS,
                     fix_ptr->longitude * TO_RADIANS,
                     fix_ptr->altitude };
   double ecef[3];
   wgsllh2ecef(llh, ecef);
   
+  // ECEF triplet is converted to north-east-down (NED), by combining it
+  // with the ECEF-formatted datum point.
   double ned[3];
   wgsecef2ned_d(ecef, ecef_datum, ned);
 
@@ -64,13 +78,22 @@ static void handle_fix(const sensor_msgs::NavSatFixConstPtr fix_ptr,
   odom_msg.pose.pose.position.x = ned[1];
   odom_msg.pose.pose.position.y = ned[0];
   odom_msg.pose.pose.position.z = -ned[2];
+
+  // We only need to populate the diagonals of the covariance matrix; the
+  // rest initialize to zero automatically, which is correct as the
+  // dimensions of the state are independent.
   odom_msg.pose.covariance[0] = fix_ptr->position_covariance[0];
   odom_msg.pose.covariance[7] = fix_ptr->position_covariance[4];
   odom_msg.pose.covariance[14] = fix_ptr->position_covariance[8];
-  odom_msg.pose.covariance[21] = 1000;
-  odom_msg.pose.covariance[28] = 1000;
-  odom_msg.pose.covariance[35] = 1000;
+  
+  // Do not use/trust orientation dimensions from GPS.
+  odom_msg.pose.covariance[21] = 1e6;
+  odom_msg.pose.covariance[28] = 1e6;
+  odom_msg.pose.covariance[35] = 1e6;
+
+  // Orientation of GPS is pointing upward (as far as we know).
   odom_msg.pose.pose.orientation.w = 1;
+
   pub_enu.publish(odom_msg); 
 }
 
@@ -80,6 +103,8 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "from_fix");
   ros::NodeHandle n;
 
+  // Initialize publishers, and pass them into the handler for 
+  // the subscriber.
   ros::Publisher pub_enu = n.advertise<nav_msgs::Odometry>("enu", 5);
   ros::Publisher pub_datum = n.advertise<sensor_msgs::NavSatFix>("enu_datum", 5, true);
   ros::Subscriber sub = n.subscribe<sensor_msgs::NavSatFix>("fix", 5, 
--- src/to_fix.cpp
+++ src/to_fix.cpp
@@ -7,6 +7,7 @@
 #include <boost/bind.hpp>
 
 extern "C" {
+  // The backend of this node is an included C library called libswiftnav.
   #include "coord_system.h"
 }
 
@@ -18,6 +19,8 @@ static void handle_enu(const nav_msgs::OdometryConstPtr odom_ptr,
                        const double ecef_datum[3],
                        const ros::Publisher& pub_fix)
 {
+  // Prepare NED vector from ENU coordinates, perform conversion in libswiftnav
+  // library calls.
   double ned[3] = { odom_ptr->pose.pose.position.y,
                     odom_ptr->pose.pose.position.x
                     -odom_ptr->pose.pose.position.z };
@@ -28,14 +31,21 @@ static void handle_enu(const nav_msgs::OdometryConstPtr odom_ptr,
   double llh[3];
   wgsecef2llh(ecef, llh);
 
+  // Prepare output Fix message. Copy over timestamp from source message,
+  // convert radian latlon output back to degrees.
   sensor_msgs::NavSatFix fix_msg;
   fix_msg.header.stamp = odom_ptr->header.stamp;
   fix_msg.latitude = llh[0] * TO_DEGREES;
   fix_msg.longitude = llh[1] * TO_DEGREES;
   fix_msg.altitude = llh[2];
+
+  // We only need to populate the diagonals of the covariance matrix; the
+  // rest initialize to zero automatically, which is correct as the
+  // dimensions of the state are independent.
   fix_msg.position_covariance[0] = odom_ptr->pose.covariance[0];
   fix_msg.position_covariance[4] = odom_ptr->pose.covariance[7];
   fix_msg.position_covariance[8] = odom_ptr->pose.covariance[14];
+
   pub_fix.publish(fix_msg); 
 }
 
@@ -43,13 +53,15 @@ static void handle_enu(const nav_msgs::OdometryConstPtr odom_ptr,
 static void handle_datum(const sensor_msgs::NavSatFixConstPtr datum_ptr,
                          ros::NodeHandle& n)
 {
+  // Convert received datum to latlon using function from libswiftnav.
   double llh[3] = { datum_ptr->latitude * TO_RADIANS,
                     datum_ptr->longitude * TO_RADIANS,
                     datum_ptr->altitude };
-
   static double ecef_datum[3];
   wgsllh2ecef(llh, ecef_datum);
 
+  // Pass datum into the data subscriber. Subscriber, Publisher, and the datum
+  // array are static, so that they stick around when this function has exited.
   static ros::Publisher pub_fix = n.advertise<sensor_msgs::NavSatFix>("fix", 5);
   static ros::Subscriber sub_enu = n.subscribe<nav_msgs::Odometry>("enu", 5, 
       boost::bind(handle_enu, _1, ecef_datum, boost::ref(pub_fix)));
@@ -61,6 +73,8 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "to_fix");
   ros::NodeHandle n;
 
+  // Initially just subscribe to the datum topic. Once we receive that, we'll
+  // spin up the rest of the node's activities.
   ros::Subscriber sub_datum = n.subscribe<sensor_msgs::NavSatFix>("enu_datum", 5,
       boost::bind(handle_datum, _1, boost::ref(n)));
 
